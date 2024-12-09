44df4eb7c942f7337df7ae915f07009bdf56edc4
Nabaa <bit23_1_50@uoitc.edu.iq>
2024-12-09 16:58:35 +0300

    y

---
+++ Migrations/20241209133720_kk.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StartUpApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class kk : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "created_at",
+                table: "Users",
+                newName: "createdAt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "createdAt",
+                table: "Users",
+                newName: "created_at");
+        }
+    }
+}
